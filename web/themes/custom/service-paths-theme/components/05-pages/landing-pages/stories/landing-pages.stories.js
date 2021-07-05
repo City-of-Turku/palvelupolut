c9cee78c89992495a257f722226f769fc808e0d7
Dāniels Brūvers <daniels.bruvers@wunder.io>
2021-07-05 12:44:03 +0300

    Remove most of the unused components

--- web/themes/custom/service-paths-theme/components/05-pages/landing-pages/stories/landing-pages.stories.js
+++ web/themes/custom/service-paths-theme/components/05-pages/landing-pages/stories/landing-pages.stories.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { ComponentWrapper } from '../../../storybook/storybook';
 
-import '../../../02-molecules/navigation/menu-main/menu-main';
-
 import home from './home.twig';
 
-import mainMenuData from '../../../02-molecules/navigation/menu-main/stories/menu-main.yml';
 import languageMenuData from '../../../02-molecules/navigation/language-switcher/stories/language-switcher.yml';
 import breadcrumbData from '../../../02-molecules/navigation/breadcrumbs/stories/breadcrumbs.yml';
-import socialMenuData from '../../../02-molecules/navigation/menu-social/stories/menu-social.yml';
 
 /**
  * Storybook Definition.
@@ -18,10 +14,8 @@ export default { title: 'Pages/Landing Pages' };
 export const homePage = () => (
   <ComponentWrapper
     markup={home({
-      ...mainMenuData,
       ...languageMenuData,
       ...breadcrumbData,
-      ...socialMenuData,
     })}
   />
 );
