be2a1c161d36c1bf99eaf22937750f2fd261d1bb
Gatis Rudins <>
2021-08-25 21:25:42 +0300

    PTV: Migration: Unpublish missing

--- web/modules/custom/ptv/src/EventSubscriber/PtvMigrationEvents.php
+++ web/modules/custom/ptv/src/EventSubscriber/PtvMigrationEvents.php
@@ -30,8 +30,12 @@ class PtvMigrationEvents implements EventSubscriberInterface {
   public function unpublishMissing(MigrateImportEvent $event) {
     $migration = $event->getMigration();
     $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    $site_languages = \Drupal::languageManager()->getLanguages();
+    foreach ($site_languages as $lang) {
+      $languages[$lang->getId()] = $lang->getId();
+    }
 
-    if ($migration->id() === 'services_translations') {
+    if ($migration->id() === 'services' || $migration->id() === 'service_channels') {
       $id_map = $migration->getIdMap();
       $id_map->prepareUpdate();
       // Clone so that any generators aren't initialized prematurely.
@@ -47,17 +51,22 @@ class PtvMigrationEvents implements EventSubscriberInterface {
       while ($id_map->valid()) {
         $map_source_id = $id_map->currentSource();
         if (!in_array($map_source_id, $source_id_values, TRUE)) {
-          // $destination_ids = $id_map->currentDestination();
-          // if (!empty($destination_ids['nid'])) {
-          //   $node = $nodeStorage->load($destination_ids['nid']);
-          //   if ($node instanceof NodeInterface) {
-          //     $status = $node->get('status')->getString();
-          //     if ($status == '1') {
-          //       $node->set('status', 0);
-          //       $node->save();
-          //     }
-          //   }
-          // }
+          $destination_ids = $id_map->currentDestination();
+          if (!empty($destination_ids['nid'])) {
+            $node = $nodeStorage->load($destination_ids['nid']);
+            if ($node instanceof NodeInterface) {
+              foreach ($languages as $lang_code) {
+                if ($node->hasTranslation($lang_code)) {
+                  $node = $node->getTranslation($lang_code);
+                  $status = $node->get('status')->getString();
+                  if ($status == '1') {
+                    $node->set('status', 0);
+                    $node->save();
+                  }
+                }
+              }
+            }
+          }
         }
         $id_map->next();
       }
