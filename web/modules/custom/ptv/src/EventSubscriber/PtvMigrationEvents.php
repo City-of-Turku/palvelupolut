faa3ffe4ec7d084f024162d9683c67c09f878051
Gatis Rudins <>
2021-08-26 10:17:49 +0300

    PTV: empty field values check; tmp disable unpublishing check

--- web/modules/custom/ptv/src/EventSubscriber/PtvMigrationEvents.php
+++ web/modules/custom/ptv/src/EventSubscriber/PtvMigrationEvents.php
@@ -28,49 +28,49 @@ class PtvMigrationEvents implements EventSubscriberInterface {
    *   The migration import event.
    */
   public function unpublishMissing(MigrateImportEvent $event) {
-    $migration = $event->getMigration();
-    $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
-    $site_languages = \Drupal::languageManager()->getLanguages();
-    foreach ($site_languages as $lang) {
-      $languages[$lang->getId()] = $lang->getId();
-    }
+    // $migration = $event->getMigration();
+    // $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    // $site_languages = \Drupal::languageManager()->getLanguages();
+    // foreach ($site_languages as $lang) {
+    //   $languages[$lang->getId()] = $lang->getId();
+    // }
 
-    if ($migration->id() === 'services' || $migration->id() === 'service_channels') {
-      $id_map = $migration->getIdMap();
-      $id_map->prepareUpdate();
-      // Clone so that any generators aren't initialized prematurely.
-      $source = clone $migration->getSourcePlugin();
-      $source->rewind();
-      $source_id_values = [];
-      while ($source->valid()) {
-        $source_id_values[] = $source->current()->getSourceIdValues();
-        $source->next();
-      }
-      $id_map->rewind();
+    // if ($migration->id() === 'services' || $migration->id() === 'service_channels') {
+    //   $id_map = $migration->getIdMap();
+    //   $id_map->prepareUpdate();
+    //   // Clone so that any generators aren't initialized prematurely.
+    //   $source = clone $migration->getSourcePlugin();
+    //   $source->rewind();
+    //   $source_id_values = [];
+    //   while ($source->valid()) {
+    //     $source_id_values[] = $source->current()->getSourceIdValues();
+    //     $source->next();
+    //   }
+    //   $id_map->rewind();
 
-      while ($id_map->valid()) {
-        $map_source_id = $id_map->currentSource();
-        if (!in_array($map_source_id, $source_id_values, TRUE)) {
-          $destination_ids = $id_map->currentDestination();
-          if (!empty($destination_ids['nid'])) {
-            $node = $nodeStorage->load($destination_ids['nid']);
-            if ($node instanceof NodeInterface) {
-              foreach ($languages as $lang_code) {
-                if ($node->hasTranslation($lang_code)) {
-                  $node = $node->getTranslation($lang_code);
-                  $status = $node->get('status')->getString();
-                  if ($status == '1') {
-                    $node->set('status', 0);
-                    $node->save();
-                  }
-                }
-              }
-            }
-          }
-        }
-        $id_map->next();
-      }
-    }
+    //   while ($id_map->valid()) {
+    //     $map_source_id = $id_map->currentSource();
+    //     if (!in_array($map_source_id, $source_id_values, TRUE)) {
+    //       $destination_ids = $id_map->currentDestination();
+    //       if (!empty($destination_ids['nid'])) {
+    //         $node = $nodeStorage->load($destination_ids['nid']);
+    //         if ($node instanceof NodeInterface) {
+    //           foreach ($languages as $lang_code) {
+    //             if ($node->hasTranslation($lang_code)) {
+    //               $node = $node->getTranslation($lang_code);
+    //               $status = $node->get('status')->getString();
+    //               if ($status == '1') {
+    //                 $node->set('status', 0);
+    //                 $node->save();
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //     $id_map->next();
+    //   }
+    // }
   }
 
 }
