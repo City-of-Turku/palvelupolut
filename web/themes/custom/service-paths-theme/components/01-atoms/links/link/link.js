69bd21551acfc48b52927dd65e78f54466d92814
Gatis Rudins <>
2021-09-14 14:14:58 +0300

    External links in text fields

--- web/themes/custom/service-paths-theme/components/01-atoms/links/link/link.js
+++ web/themes/custom/service-paths-theme/components/01-atoms/links/link/link.js
@@ -16,6 +16,30 @@
           $('.link--back-to-top').removeClass('js-show');
         }
       });
+
+      $(document).ready(function () {
+        const $mailLinks = $('a[href^="mailto:"]');
+        const $telLinks = $('a[href^="tel:"]');
+        // exclude mail-links and loop through the rest
+        $('.text-long a')
+          .once()
+          .not($mailLinks)
+          .not($telLinks)
+          .each(function () {
+            // skip if link contains host url since its an internal link
+            if (String(this.href).indexOf(window.location.host) == -1) {
+              // Check if it is an internal anchor without href
+              const hashref = this.href;
+              if (hashref && hashref != 'javascript:void(0);') {
+                // add class for css and set target to blank to open external links in new window
+                $(this)
+                  .addClass('link--external--text')
+                  .attr('target', '_blank')
+                  .append('<i class="fas fa-external-link-alt"></i>');
+              }
+            }
+          });
+      });
     },
   };
 })(jQuery, Drupal, drupalSettings);
