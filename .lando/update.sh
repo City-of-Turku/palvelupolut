4768dcbda6157c83b52089304d06dbcacc693fb1
Tormi Tabor <tormi.tabor@gmail.com>
2022-03-18 23:14:38 +0200

    TKU-537: Enable dblog locally.

--- .lando/update.sh
+++ .lando/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -exu
 
-# Enable development modules.
-drush @local en devel update stage_file_proxy -y
-
 # Disable Warden module.
-drush @local pmu warden -y
+drush @local pmu dblog warden -y
+
+# Enable development modules.
+drush @local en dblog devel update stage_file_proxy -y
 
 # Update the local database.
 drush @local updb -y
