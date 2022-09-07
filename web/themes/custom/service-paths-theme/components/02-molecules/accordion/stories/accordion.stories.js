eb630a6d6eec680274b03cc6fe22a812ab7c0d55
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2022-09-07 13:16:26 +0300

    TURKU-6: Adding the new accordion variant 'aside' to Storybook.

--- web/themes/custom/service-paths-theme/components/02-molecules/accordion/stories/accordion.stories.js
+++ web/themes/custom/service-paths-theme/components/02-molecules/accordion/stories/accordion.stories.js
@@ -8,8 +8,9 @@ import accordionTemplate from '../accordion.twig';
 
 import accordionData from './accordion.yml';
 import accordionWithHeadingData from './accordion-with-heading.yml';
-import warningAccordionData from './warning-accordion.yml';
+import accordionWarningData from './accordion-warning.yml';
 import accordionWhiteData from './accordion-white.yml';
+import accordionAsideData from './accordion-aside.yml';
 
 /**
  * Storybook Definition.
@@ -27,12 +28,16 @@ export const accordion = () => (
       label="Accordion, with heading"
     />
     <ComponentWrapper
-      markup={accordionTemplate(warningAccordionData)}
+      markup={accordionTemplate(accordionWarningData)}
       label="Warning accordion"
     />
     <ComponentWrapper
       markup={accordionTemplate(accordionWhiteData)}
       label="White accordion"
+    />
+    <ComponentWrapper
+      markup={accordionTemplate(accordionAsideData)}
+      label="Aside accordion"
     />
   </CollectionWrapper>
 );
