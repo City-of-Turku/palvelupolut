9c635d8dd93a895c746ee068cf2a132240c2bc2c
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2022-12-01 15:14:35 +0200

    TURKU-12: Updating content summary mock data by replacing fixed contact-card html with one generated via contact-card component.

--- web/themes/custom/service-paths-theme/components/02-molecules/content-summary/stories/content-summary.stories.js
+++ web/themes/custom/service-paths-theme/components/02-molecules/content-summary/stories/content-summary.stories.js
@@ -6,9 +6,14 @@ import {
 } from '../../../storybook/storybook';
 
 import contentSummaryTemplate from '../content-summary.twig';
-
 import contentSummaryData from './content-summary.yml';
 import contentSummaryTwoColumnsData from './content-summary-two-columns.yml';
+import contactCardTemplate from '../../contact-card/contact-card.twig';
+import contactCardOpeningHoursData from '../../contact-card/stories/contact-card-opening-hours.yml';
+
+contentSummaryTwoColumnsData.content_summary__lead_text = contactCardTemplate(
+  contactCardOpeningHoursData,
+);
 
 /**
  * Storybook Definition.
